d2b4ede7360c61365ee414c25b25924778f7b585
Geoff Stanley <geoffstanley@gmail.com>
2021-09-23 20:56:04 +1000

    doco update

--- lib/ntp-bottle-to-cast/ntp_bottle_to_cast.m
+++ lib/ntp-bottle-to-cast/ntp_bottle_to_cast.m
@@ -1,19 +1,20 @@
 function [p, s, t, success] = ntp_bottle_to_cast(Sppc, Tppc, P, k, sB, tB, pB, tolp) %#codegen
-%NTP_BOTTLE_TO_CAST  Find a bottle's level of neutral buoyancy in a water
-%                    column, using the Neutral Tangent Plane relationship.
+%NTP_BOTTLE_TO_CAST  Find a bottle's level of neutral buoyancy in a cast,
+%                    using the Neutral Tangent Plane relationship.
 %
 % [p, s, t] = ntp_bottle_to_cast(Sppc, Tppc, P, k, sB, tB, pB, tolp)
-% finds (s, t, p), with precision in p of tolp, that is at the level of
+% finds (s, t, p), with accuracy in p of tolp, that is at the level of
 % neutral buoyancy for a fluid bottle of (sB, tB, pB) in a water column of
 % with piecewise polynomial interpolants for S and T given by Sppc and Tppc 
 % with knots at P(1:k).  Specifically, s and t are given by
-%   [s,t] = ppc_val(P, Sppc, Tppc, p)
+%   [s,t] = ppc_val2(P, Sppc, Tppc, p)
 % and p satisfies
 %      eos(s, t, p') = eos(sB, tB, p')
 % where eos is the equation of state given by eos.m in MATLAB's path,
 % and   p' is in the range [p_avg - tolp/2, p_avg + tolp/2],
 % and   p_avg = (pB + p) / 2 is the average of the fluid bottle's original
 %                          and final pressure or depth.
+% The equation of state is determined by eos.m on the MATLAB path. 
 %
 % [p, s, t, success] = ntp_bottle_to_cast(...)
 % returns a flag value success that is true if a valid solution was found,
@@ -24,30 +25,36 @@ function [p, s, t, success] = ntp_bottle_to_cast(Sppc, Tppc, P, k, sB, tB, pB, t
 %
 %
 % --- Input:
-% Sppc [O, K-1]: coefficients for piecewise polynomial for practical 
-%                   / Absolute Salinity in terms of P
-% Tppc [O, K-1]: coefficients for piecewise polynomial for potential 
-%                   / Conservative Temperature in terms of P
-% P [K, 1]: pressure or depth in water column
-% k [1, 1]: number of valid (non-NaN) data points in the water column.
-%          Specifically, Sppc(end,1:k) and Tppc(end,1:k) must all be valid.
+% Sppc [O, K-1]: coefficients for piecewise polynomial for practical / 
+%     Absolute Salinity in terms of Z, on casts A and B.  Here, O is the
+%     polynomial order and K is the number of data points (including NaN's)
+%     on the casts.
+% Tppc [O, K-1]: as above but for potential / Conservative Temperature.
+% P [K, 1]: pressure or depth data points on the cast.
+% k [1, 1]: number of valid (non-NaN) data points on the cast.
+%          Specifically, Sppc(:,1:k-1) and Tppc(:,1:k-1) must be non-NaN. 
 % sB [1 , 1]: practical / Absolute salinity of current bottle
 % tB [1 , 1]: potential / Conservative temperature of current bottle
 % pB [1 , 1]: pressure or depth of current bottle
 % tolp [1, 1]: tolerance for solving the level of neutral buoyancy (same
-%             units as P and pB)
+%              units as P and pB)
 %
-% Note: physical units for Sppc, Tppc, P, sB, tB, pB, p, s, t  are
+% Note: physical units for Sppc, Tppc, P, sB, tB, pB, p, s, t are
 % determined by eos.m.
 %
 % Note: P must increase monotonically along its first dimension.
 %
 %
 % --- Output:
-% p [1, 1]: pressure or depth in water column at level of neutral buoyancy
-% s [1, 1]: practical / Absolute salinity in water column at level of neutral buoyancy
-% t [1, 1]: potential / Conservative temperature in water column at level of neutral buoyancy
+% p [1, 1]: pressure or depth in the cast at level of neutral buoyancy
+% s [1, 1]: practical / Absolute salinity in the cast at level of neutral buoyancy
+% t [1, 1]: potential / Conservative temperature in the cast at level of neutral buoyancy
 % success [1,1]: true if a valid solution was found, false otherwise.
+%
+%
+% --- See Also:
+% ntp_midpoint_to_casts
+% ppc_linterp, ppc_pchip
 
 % Author(s) : Geoff Stanley
 % Email     : g.stanley@unsw.edu.au
